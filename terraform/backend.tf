45ccc4c4707012cde98f3bd31f1f9ef8a86d7444
razbensimon <raz.bensimon@env0.com>
2022-11-21 18:31:25 +0200

    tfc config

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,11 +2,10 @@ terraform {
   required_version  = ">= 1.3.4"
 
   backend "remote" {
-    hostname = "backend.api.env0.com"
-    organization = "1ffc3f01-9501-4577-87e7-6ea8e5b4d74b.bac0dd9a-23e7-4d17-a726-4931ef301eac"
+    organization = "raz-remote-be"
 
     workspaces {
-      name = "my-backend-env-1"
+      prefix = "my-backend-env-"
     }
   }
 }
