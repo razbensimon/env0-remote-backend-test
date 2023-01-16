1b9e2488dbed302d5a0fe10afb4acc1e3456d7cf
razbensimon <raz.bensimon@env0.com>
2023-01-16 21:35:40 +0200

    fix 1.3.4 crash issue

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,12 +1,12 @@
 terraform {
-  required_version  = ">= 1.3.4"
+  required_version = ">= 1.3.6"
 
   backend "remote" {
     hostname = "backend.api.env0.com"
     organization = "1ffc3f01-9501-4577-87e7-6ea8e5b4d74b.bac0dd9a-23e7-4d17-a726-4931ef301eac"
 
     workspaces {
-      name = "my-backend-env-1"
+      prefix = "my-first-remote-plan-env-"
     }
   }
 }
